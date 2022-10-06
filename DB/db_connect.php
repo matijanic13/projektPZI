efe02b5ae7a677197ce1a69229695b2700fa66b3
matijanic13 <90564543+matijanic13@users.noreply.github.com>
2022-10-06 10:22:11 +0200

    Update db_connect.php

--- DB/db_connect.php
+++ DB/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 function dbConnect() {
-    $db = mysqli_connect('localhost', 'root', '', 'projektpzi');
+    $db = mysqli_connect('studnti.sum.ba', 'pzi262022', 'csdigital2022', 'pzi262022');
     return $db;
 }
 
